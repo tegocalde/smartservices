fa417489d428f73b9bb324a846503b306e906dc1
tegocalde <superchobi@hotmail.es>
2019-03-05 18:51:00 -0400

    prueba inicio en la pc de escritorio

---
+++ src/pages/seguimiento/seguimiento.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { SeguimientoPage } from './seguimiento';
+
+@NgModule({
+  declarations: [
+    SeguimientoPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(SeguimientoPage),
+  ],
+})
+export class SeguimientoPageModule {}
